3db38d5353c4b54fd0a4b755528328c50332526b
sharlinekerubo27@gmail.com <sharlinekerubo27@gmail.com>
2025-07-02 13:00:05 +0300

    Fixed the query issue

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -8,11 +8,17 @@ GROUP BY
     user_id;
 
 -- Rank properties based on total bookings
+-- Rank properties by the total number of bookings
 SELECT 
     property_id,
-    COUNT(*) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
-FROM 
-    Booking
-GROUP BY 
-    property_id;
+    total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+    SELECT 
+        property_id,
+        COUNT(*) AS total_bookings
+    FROM 
+        Booking
+    GROUP BY 
+        property_id
+) AS property_bookings;
