71e9ff128c091101f9dff4f70e053c9e4e159321
sharlinekerubo27@gmail.com <sharlinekerubo27@gmail.com>
2025-07-02 13:07:17 +0300

    update queries

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -7,11 +7,12 @@ FROM
 GROUP BY 
     user_id;
 
--- Rank properties by total number of bookings using ROW_NUMBER
+-- Rank properties by total number of bookings using ROW_NUMBER AND RANK
+
 SELECT 
     property_id,
     total_bookings,
-    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_num
 FROM (
     SELECT 
         property_id,
@@ -21,3 +22,19 @@ FROM (
     GROUP BY 
         property_id
 ) AS property_bookings;
+
+-- Rank properties using RANK() by total number of bookings
+SELECT 
+    property_id,
+    total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+    SELECT 
+        property_id,
+        COUNT(*) AS total_bookings
+    FROM 
+        Booking
+    GROUP BY 
+        property_id
+) AS property_bookings;
+
